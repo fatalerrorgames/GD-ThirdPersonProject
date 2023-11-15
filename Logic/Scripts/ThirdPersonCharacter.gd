515f15147bf0146c453f8039a8c280b0159246da
Timon Zimmermann <32746328+fatalerrorgames@users.noreply.github.com>
2023-11-15 22:55:21 +0100

    Smooth Camera
    also some animation fixes

--- Logic/Scripts/ThirdPersonCharacter.gd
+++ Logic/Scripts/ThirdPersonCharacter.gd
@@ -1,4 +1,15 @@
 extends CharacterBody3D
 
+@onready var yLook = $yLook
+@onready var xLook = $yLook/xLook
+
+var yLookTransform
+var xLookTransform
+
+
 func _physics_process(delta):
 	move_and_slide()
+
+func _process(delta):
+	yLookTransform = yLook.global_transform.origin
+	xLookTransform = xLook.global_transform.basis
