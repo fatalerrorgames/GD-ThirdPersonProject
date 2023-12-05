04511fafaba826eb8d69325525aeddd25b7946cf
Timon Zimmermann <32746328+fatalerrorgames@users.noreply.github.com>
2023-12-05 22:09:28 +0100

    deleted old Model nodes, implemented particles

--- Logic/Scripts/Dummy.gd
+++ Logic/Scripts/Dummy.gd
@@ -5,6 +5,8 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var PlayerReference = CharacterBody3D
 var direction = Vector3(0,0,0)
 
+var hit_fx = preload("res://Assets/FX/enemy_hit_particles.tscn")
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -18,3 +20,10 @@ func _physics_process(delta):
 
 func _on_hit_box_area_entered(area):
 	velocity = (PlayerReference.velocity.normalized()*12)
+	instance_object()
+
+func instance_object():
+
+	var instance = hit_fx.instantiate()
+	#call_deferred("add_child",object)
+	add_child(instance)
