2c17dd64c7fae923f39f482a4a8f77aa53198d90
Timon Zimmermann <32746328+fatalerrorgames@users.noreply.github.com>
2023-12-12 12:03:38 +0100

    optimisations
    particles now get destroyed when not active

--- Levels/QuitGame.gd
+++ Levels/QuitGame.gd
@@ -13,6 +13,5 @@ func _process(delta):
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode ==KEY_ESCAPE:
-			#get_tree().quit()
 			get_tree().change_scene_to_file("res://Levels/Menu.tscn")
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
